0028a497a4a940e8dd37eb772e86c5e8ddc2a094
Motiejus Slimas <motiejusslim@gmail.com>
2023-12-07 17:56:27 +0200

    Fixed banned and logout token remove.

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,23 +1,35 @@
 import {Navigate, Outlet, useNavigate} from "react-router-dom";
 import {useAuth} from "./provider/authProvider";
 import axiosInstance from "./api/customAxios";
+import {useState} from "react";
 
 export const ProtectedRoute = () => {
+    const [checkedIfBanned, setCheckedIfBanned] = useState(false)
     const { token, setToken, setRefreshToken } = useAuth();
     const navigate =  useNavigate()
+
+    const checkIfBanned = () => {
+        if(!checkedIfBanned) {
+            console.log(token)
+            axiosInstance.get("api/v1/users/amibanned")
+                .then(res => {
+                    if(res.data) {
+                        setToken()
+                        setRefreshToken()
+                        navigate('/banned')
+                    } else  {
+                        setCheckedIfBanned(true)
+                    }
+                })
+        }
+    }
 
     if (!token) {
         return <Navigate to="/login" />;
     }
 
-    axiosInstance.get("api/v1/users/amibanned")
-        .then(res => {
-            if(res.data) {
-                setToken()
-                setRefreshToken()
-                navigate('/banned')
-            }
-        })
+    checkIfBanned()
+
 
     return <Outlet />;
 };
